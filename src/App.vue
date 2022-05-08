78036023abd61dff830f9cd411d9aa5680cfc58e
CoderHYJ <zm1178466304@gmail.com>
2022-05-09 00:46:13 +0800

    搭建项目的目录结构

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h2>你好,世界!</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  @import "./assets/css/base.css"
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
+// 这个配置文件的配置会和公共的配置进行合并
 module.exports = {
   configureWebpack: {
     resolve: {
+      // 别名,给src文件夹下的文件夹配置别名
+      //router&store不需要,因为可以在文件中获取相应的值
       alias: {
         'assets': '@/assets',
         'common': '@/common',
